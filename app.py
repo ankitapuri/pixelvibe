7f281c45ab0d397ac13318480a5908569d84bace
arpit456jain <111arpit1@gmail.com>
2021-05-13 17:53:43 +0530

    db connect done

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask,render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+import json
+
 app = Flask(__name__,template_folder='template')
+
+#now we use config.json
+with open('config.json','r') as c:
+    params = json.load(c)['params']
+local_server = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+db = SQLAlchemy(app)
+class Users(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+
 
 @app.route('/',methods = ['POST', 'GET'])
 def hello_world():
