17a79c722b17a7ec5896039c4bb2752e9a43743c
arpit456jain <111arpit1@gmail.com>
2021-05-28 21:08:28 +0530

    fixed a bug

--- static/JS/showPassword.js
+++ static/JS/showPassword.js
@@ -20,15 +20,3 @@ const togglePassword2 = document.querySelector('#togglePassword2');
 			this.classList.toggle('fa-eye-slash');
 		});
 
-
-
-// for show password for one
-const togglePassword1 = document.querySelector('#togglePassword1');
-const password1 = document.querySelector('#password1');
-togglePassword1.addEventListener('click', function (e) {
-// toggle the type attribute
-const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
-password1.setAttribute('type', type);
-// toggle the eye slash icon
-this.classList.toggle('fa-eye-slash');
-});
