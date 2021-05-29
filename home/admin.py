1c7752f6b437ae67ec8360082d632e5e8333238d
111arpit1 <55352601+arpit456jain@users.noreply.github.com>
2021-05-29 22:50:48 +0530

    added download csv feature (#328)

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin
+# Register your models here.
 # Register your models here.
 # # from home.models import 
 from home.models import Contact,Gallery
-admin.site.register((Contact,Gallery))
+admin.site.register(Gallery)
+
+@admin.register(Contact)
+class FeedBackAdmin(ImportExportModelAdmin):
+    pass
