16a5b9cee5404ba46cd9b673a758c11441fb5ebb
111arpit1 <55352601+arpit456jain@users.noreply.github.com>
2021-05-30 18:17:05 +0530

    download csv feature (#338)
    * added download csv feature
    
    * update requirements.txt

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
-# # from home.models import 
+from import_export.admin import ImportExportModelAdmin
 from home.models import Contact,Gallery
-admin.site.register((Contact,Gallery))
+admin.site.register(Gallery)
+
+@admin.register(Contact)
+class ContactAdmin(ImportExportModelAdmin):
+    pass
